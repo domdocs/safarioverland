7755e8673befbcd7238adc5b56334ad5e003d256
Dominic Doherty <dominicdoherty@Dominics-MacBook-Air.local>
2025-04-18 17:18:16 +0200

    Fix deployment issues and add Zimbabwe Rhinos conservation success story

---
+++ components/CookieConsent.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export const CookieConsent = () => {
+  return (
+    <div className="hidden">
+      {/* Empty placeholder component */}
+    </div>
+  )
+}
+
+export default CookieConsent 
