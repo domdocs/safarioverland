79a1c3cd615232840146f86ba708d5aab830abf4
domdocs <domdocs@icloud.com>
2025-04-22 20:39:15 +0200

    Add Header and Footer to Contact page layout

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,3 +1,5 @@
+import { Header } from "@/components/header"
+import { Footer } from "@/components/footer"
 import { ToastProvider } from "./toast-provider"
 
 export default function ContactLayout({
@@ -6,9 +8,13 @@ export default function ContactLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      {children}
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
       <ToastProvider />
-    </>
+    </div>
   )
 } 
